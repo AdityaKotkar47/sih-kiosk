914b2db2347cf60073fe1dcf09f11d93a475a665
aditya <adityakotkar47@gmail.com>
2024-12-04 13:55:16 +0530

    remove loading

--- src/pages/Loading.tsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <img src={require('../assets/img/pravaah-logo.png')} alt="Pravaah Logo" className="w-32 h-32 animate-spin" />
-    </div>
-  );
-}
-
-export default Loading;
